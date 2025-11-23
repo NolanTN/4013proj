0d7cac403fbfdad4e60d1a6455374e0d20e4156e
Parker Davison <Parker.L.Davison-1@ou.edu>
2025-11-23 16:39:02 -0600

    Apply and layout set up

---
+++ 4013Project/4013Project/Controllers/ApplyController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _4013Project.Controllers
+{
+    public class ApplyController : Controller
+    {
+        // GET: /Apply
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new _4013Project.Models.Apply();
+            return View(model);
+        }
+
+        // POST: /Apply
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(_4013Project.Models.Apply model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Return the same view with validation messages
+                return View(model);
+            }
+
+            // TODO: save to database / send email / whatever your process is
+
+            // Maybe redirect to a confirmation page
+            return RedirectToAction("Confirmation");
+        }
+
+        // GET: /Apply/Confirmation
+        [HttpGet]
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+}
---
+++ 4013Project/4013Project/Models/Apply.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _4013Project.Models
+{
+    public class Apply
+    {
+        // ---- Company Info ----
+        [Display(Name = "Company Type")]
+        [Required(ErrorMessage = "Please select a company type.")]
+        public string CompanyType { get; set; }   // "IBC" or "Independent"
+
+        [Display(Name = "Link to Company Page")]
+        [Url(ErrorMessage = "Please enter a valid URL.")]
+        public string CompanyLink { get; set; }
+
+        [Required, Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required, Display(Name = "City")]
+        public string City { get; set; }
+
+        [Required, Display(Name = "State")]
+        public string State { get; set; }
+
+        [Required, Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, Display(Name = "Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        // ---- Product Info ----
+        [Required, Display(Name = "Brand Name")]
+        public string BrandName { get; set; }
+
+        [Required, Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Product Image URL")]
+        public string ProductImageUrl { get; set; }   // or use IFormFile in controller
+
+        [Required, Display(Name = "Product Description")]
+        public string ProductDescription { get; set; }
+
+        [Required, Range(0, 1000000)]
+        public decimal? Price { get; set; }
+    }
+}
